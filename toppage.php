6e0afbc5506903fa1117b2d0ac89569623238241
【kiwatchi1991】 <ymnkknt301@gmail.com>
2019-06-28 11:09:25 +0900

    second commit

--- toppage.php
+++ toppage.php
@@ -52,29 +52,91 @@ require('head.php');
   require('header.php');
   ?>
 
-  <div class="msg">
-    <p id="js-show-msg" style="display:none;" class="msg-slide">
-      <?php echo getSessionFlash('msg_success'); ?>
-    </p>
-  </div>
+    <div class="msg">
+      <p id="js-show-msg" style="display:none;" class="msg-slide">
+        <?php echo getSessionFlash('msg_success'); ?>
+      </p>
+    </div>
 
-  <!--  広告タブ-->
-  <?php 
-  require('ads.php');
-  ?>
+    <!--  広告タブ-->
+    <?php 
+    require('ads.php');
+    ?>
 
-  <!--メニュータブ-->
-  <?php 
-  require('menuTab.php');
-  ?>
+    <!--メニュータブ-->
+    <?php 
+    require('menuTab.php');
+    ?>
 
-  <div class="site-width">
+    <!-- メインコンテンツ -->
+  <div id="contents" class="site-width">
+    
+<!--    サイドバー-->
+    <section id="sidebar">
+      <form name="" method="get">
+        <h1 class="title">カテゴリー</h1>
+        <div class="selectbox">
+          <span class="icn_select"></span>
+            <select name="c-id" id="">
+              <option value="0" <?php if(getFormData('c_id',true) == 0 ){ echo 'selected'; } ?> >選択してください</option>
+              <?php 
+                foreach($dbCategoryData as $key => $val){
+              ?>
+              <option value="<?php echo $val['id'] ?>" <?php if(getFormData('c_id',true) == $val['id'] ){ echo 'selected'; } ?> >
+                <?php echo $val['name']; ?>
+              </option>
+              <?php 
+                }
+              ?>
+            </select>
+        </div>
+        <h1 class="title">表示順</h1>
+        <div class="selectbox">
+          <span class="icn_select"></span>
+            <select name="sort">
+              <option value="0" <?php if(getFormData('soty',true) == 0 ){ echo 'selected'; } ?> >選択してください</option>
+              <option value="1" <?php if(getFormData('sort',true) == 1 ){ echo 'selected'; } ?> >金額が安い順</option>
+              <option value="2" <?php if(getFormData('sort',true) == 2 ){ echo 'selected'; } ?> >金額が高い順</option>
+            </select>
+        </div>
+        <input type="submit" value="検索">
+      </form>
+      
+    </section>
 
-    <p>トップページです</p>
-    <p>トップページです</p>
-    <p>トップページです</p>
-    <p>トップページです</p>
-
+<!--   Main-->
+     <section id="main">
+       <div class="search-title">
+         <div class="search-left">
+           <span class="total-num"><?php echo sanitize($dbProductData['total']); ?></span>件の商品が見つかりました。
+         </div>
+         <div class="search-right">
+           <span class="num"><?php ecoh (!empty($dbProductData['data'])) ? $currentMinNum+1 : 0; ?></span> - <span class="num"><?php echo $currentMinNum+count($dbProductData['data']); ?></span>件 / <span class="num"><?php echo sanitize($dbProductData['total']); ?></span>件中
+         </div>
+       </div>
+       <div class="panel-list">
+         <?php 
+         foreach($dbProductData['data'] as $key => $val):
+         debug('デバック■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■'.print_r($dbProductData,true));
+         ?>
+           <a href="productDetail.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id'.$val['id']; ?>" class="panel">
+             <div class="panel-head">
+               <img src="<?php echo sanitize($val['pic1']); ?>" alt="<?php echo sanitize($val['name']); ?>">
+             </div>
+             <div class="panel-body">
+               <p class="panel-title"><?php echo sanitize($val['name']); ?> <span class="price">¥<?php echo sanitize(number_format($val['price'])); ?></span></p>
+             </div>
+           </a>
+           <?php 
+            endforeach;
+         ?>
+       </div>
+       
+       <?php pagination($currentPageNum, $dbProductData['total_page']); 
+       ?>
+       
+     </section>
+ 
 
   </div>
 
