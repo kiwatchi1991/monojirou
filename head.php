b04752799a3ba880531a2a9142613770630f6035
【kiwatchi1991】 <ymnkknt301@gmail.com>
2019-07-01 21:44:54 +0900

    second commi

--- head.php
+++ head.php
@@ -5,4 +5,6 @@
     <title><?php echo $siteTitle; ?> | MONOJIROU</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <!-- フォントアイコン -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 </head>
